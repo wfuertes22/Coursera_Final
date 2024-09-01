aedbfa6327a49aab2d525cb2086925cd3a33dc1c
wfuertes22 <wf69@nau.edu>
2024-09-01 00:24:02 -0700

    added components add logo

--- coursera-final/src/App.js
+++ coursera-final/src/App.js
@@ -1,4 +1,10 @@
 import './App.css';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+
+
 
 function App() {
   return (
---
+++ coursera-final/src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () =>{
+    return(
+        <header>
+            <img src="../icons_assets/Logo.jpg" alt="Little Lemon Logo" width="500" height="600"></img>
+        </header>
+    );
+}
+
+export default Header;
