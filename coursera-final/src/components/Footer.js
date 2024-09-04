125a07a47cd8894dc659dad08ad8b1350072750b
wfuertes22 <wf69@nau.edu>
2024-09-04 00:54:23 -0700

    Added styling to header and footer

--- coursera-final/src/components/Footer.js
+++ coursera-final/src/components/Footer.js
@@ -1,25 +1,27 @@
+import footerLogo from "../images/Little_Lemon_Tab_Icon_Yellow.png"
 const Footer = () =>{
     return(
         <footer>
-            <ul>
-                <p>Our Company</p>
-                <a href="../"><li>Home</li></a>
-                <a href="../About"><li>About</li></a>
-                <a href="../Menu"><li>Menu</li></a>
-                <a href="../Reservations"><li>Reservations</li></a>
-                <a href="../Order_Online"><li>Order Online</li></a>
-                <a href="../Login"><li>Login</li></a>
+            <img src={footerLogo} alt="Large Little Lemon logo" className="footerLogo"></img>
+            <ul className="footerList">
+                <p className="footerListTitle">Helpful Links</p>
+                <a href="../"><li className="footerListItem">Home</li></a>
+                <a href="../About"><li className="footerListItem">About</li></a>
+                <a href="../Menu"><li className="footerListItem">Menu</li></a>
+                <a href="../Reservations"><li className="footerListItem">Reservations</li></a>
+                <a href="../Order_Online"><li className="footerListItem">Order Online</li></a>
+                <a href="../Login"><li className="footerListItem">Login</li></a>
             </ul>
-            <ul>
-                <p>Contact</p>
-                <li>Address</li>
-                <li>Phone Number</li>
-                <li>Email</li>
+            <ul className="footerList">
+                <p className="footerListTitle">Contact</p>
+                <li className="footerListItem">Address</li>
+                <li className="footerListItem">Phone Number</li>
+                <li className="footerListItem">Email</li>
             </ul>
-            <ul>
-                <p>Social Media</p>
-                <li>Facebook</li>
-                <li>Instagram</li>
+            <ul className="footerList">
+                <p className="footerListTitle">Social Media</p>
+                <li className="footerListItem">Facebook</li>
+                <li className="footerListItem">Instagram</li>
             </ul>
         </footer>
     );
--- coursera-final/src/components/Header.js
+++ coursera-final/src/components/Header.js
@@ -2,7 +2,7 @@ import Logo from "../images/Logo.svg";
 const Header = () =>{
     return(
         <header>
-            <img src={Logo} alt="Little Lemon Logo" className="headerLogo"></img>
+            <a href="../"><img src={Logo} alt="Little Lemon Logo" className="headerLogo"></img></a>
         </header>
     );
 }
