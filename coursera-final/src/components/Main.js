aee1e5d4d0f9c0bf7ae61e271fd33520c4cc67c6
wfuertes22 <wf69@nau.edu>
2024-09-07 17:32:08 -0700

    Added delivery icon and added description to product cards with some styling

--- coursera-final/src/components/Main.js
+++ coursera-final/src/components/Main.js
@@ -2,6 +2,7 @@ import restaurantIMG from "../images/restaurantfood.jpg"
 import greekSalad from "../images/greek_salad.jpg"
 import bruchetta from "../images/Bruschetta.jpg"
 import lemonDessert from "../images/lemon_dessert.jpg"
+import deliveryIcon from "../images/delivery_Icon.png"
 
 const Main = () =>{
     return(
@@ -26,21 +27,48 @@ const Main = () =>{
                 <section id="productCardList">
                     <article className="productCard">
                         <img className="foodCardIMG" id="greekSaladIMG" src={greekSalad} alt="Greek Salad"></img>
-                        <p className="cardTitle">
-                            Greek Salad
-                        </p>
+                        <div className="cardDescriptionContainer">
+                            <p className="cardTitle">
+                                Greek Salad
+                            </p>
+                            <p className="price">
+                                $12.99
+                            </p>
+                            <p className="productDescription">
+                                The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. 
+                            </p>
+                            <a href="../Order_Online">Order a delivery <img className="deliveryIMG" src={deliveryIcon} alt="delovery icon"></img></a>
+                        </div>
                     </article>
                     <article className="productCard">
                         <img className="foodCardIMG" id="bruchettaIMG"src={bruchetta} alt="Bruchetta"></img>
-                        <p className="cardTitle">
-                            Bruchetta
-                        </p>
+                        <div className="cardDescriptionContainer">
+                            <p className="cardTitle">
+                                Bruchetta
+                            </p>
+                            <p className="price">
+                                $ 5.99
+                            </p>
+                            <p className="productDescription">
+                            Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil. 
+                            </p>
+                            <a href="../Order_Online">Order a delivery <img className="deliveryIMG" src={deliveryIcon} alt="delovery icon"></img></a>
+                        </div>
                     </article>
                     <article className="productCard">
                         <img className="foodCardIMG" id="lemonDessertIMG" src={lemonDessert} alt="Lemon Dessert"></img>
-                        <p className="cardTitle">
-                            Lemon Dessert
-                        </p>
+                        <div className="cardDescriptionContainer">
+                            <p className="cardTitle">
+                                Lemon Dessert
+                            </p>
+                            <p className="price">
+                                $ 5.00
+                            </p>
+                            <p className="productDescription">
+                                This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.
+                            </p>
+                            <a href="../Order_Online">Order a delivery <img className="deliveryIMG" src={deliveryIcon} alt="delovery icon"></img></a>
+                        </div>
                     </article>
                 </section>
             </section>
