1cd57f4988e0bcbfb3d0dd2d3336fd6b58f7f91b
wfuertes22 <wf69@nau.edu>
2024-09-08 00:46:02 -0700

    added scroll to weekly specials and changed the product list from grid to flex

--- coursera-final/src/components/Main.js
+++ coursera-final/src/components/Main.js
@@ -24,53 +24,56 @@ const Main = () =>{
                     <h2 id="specialsTitle">This weeks specials</h2>
                     <button id="onlineMenu">Online Menu</button>
                 </div>
-                <section id="productCardList">
-                    <article className="productCard">
-                        <img className="foodCardIMG" id="greekSaladIMG" src={greekSalad} alt="Greek Salad"></img>
-                        <div className="cardDescriptionContainer">
-                            <p className="cardTitle">
-                                Greek Salad
-                            </p>
-                            <p className="price">
-                                $12.99
-                            </p>
-                            <p className="productDescription">
-                                The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. 
-                            </p>
-                            <a href="../Order_Online">Order a delivery <img className="deliveryIMG" src={deliveryIcon} alt="delovery icon"></img></a>
-                        </div>
-                    </article>
-                    <article className="productCard">
-                        <img className="foodCardIMG" id="bruchettaIMG"src={bruchetta} alt="Bruchetta"></img>
-                        <div className="cardDescriptionContainer">
-                            <p className="cardTitle">
-                                Bruchetta
-                            </p>
-                            <p className="price">
-                                $ 5.99
-                            </p>
-                            <p className="productDescription">
-                            Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil. 
-                            </p>
-                            <a href="../Order_Online">Order a delivery <img className="deliveryIMG" src={deliveryIcon} alt="delovery icon"></img></a>
-                        </div>
-                    </article>
-                    <article className="productCard">
-                        <img className="foodCardIMG" id="lemonDessertIMG" src={lemonDessert} alt="Lemon Dessert"></img>
-                        <div className="cardDescriptionContainer">
-                            <p className="cardTitle">
-                                Lemon Dessert
-                            </p>
-                            <p className="price">
-                                $ 5.00
-                            </p>
-                            <p className="productDescription">
-                                This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.
-                            </p>
-                            <a href="../Order_Online">Order a delivery <img className="deliveryIMG" src={deliveryIcon} alt="delovery icon"></img></a>
-                        </div>
-                    </article>
-                </section>
+                <div className="slider">
+                    <section id="productCardList">
+                    
+                        <article className="productCard">
+                            <img className="foodCardIMG" id="greekSaladIMG" src={greekSalad} alt="Greek Salad"></img>
+                            <div className="cardDescriptionContainer">
+                                <p className="cardTitle">
+                                    Greek Salad
+                                </p>
+                                <p className="price">
+                                    $12.99
+                                </p>
+                                <p className="productDescription">
+                                    The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. 
+                                </p>
+                                <a href="../Order_Online">Order a delivery <img className="deliveryIMG" src={deliveryIcon} alt="delovery icon"></img></a>
+                            </div>
+                        </article>
+                        <article className="productCard">
+                            <img className="foodCardIMG" id="bruchettaIMG"src={bruchetta} alt="Bruchetta"></img>
+                            <div className="cardDescriptionContainer">
+                                <p className="cardTitle">
+                                    Bruchetta
+                                </p>
+                                <p className="price">
+                                    $ 5.99
+                                </p>
+                                <p className="productDescription">
+                                Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil. 
+                                </p>
+                                <a href="../Order_Online">Order a delivery <img className="deliveryIMG" src={deliveryIcon} alt="delovery icon"></img></a>
+                            </div>
+                        </article>
+                        <article className="productCard">
+                            <img className="foodCardIMG" id="lemonDessertIMG" src={lemonDessert} alt="Lemon Dessert"></img>
+                            <div className="cardDescriptionContainer">
+                                <p className="cardTitle">
+                                    Lemon Dessert
+                                </p>
+                                <p className="price">
+                                    $ 5.00
+                                </p>
+                                <p className="productDescription">
+                                    This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.
+                                </p>
+                                <a href="../Order_Online">Order a delivery <img className="deliveryIMG" src={deliveryIcon} alt="delovery icon"></img></a>
+                            </div>
+                        </article>
+                    </section>
+                </div>
             </section>
             {/*--------------------------------------------*/}
             <section className="testimonials">
