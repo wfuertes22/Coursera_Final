46e0981b3d51b90d2214ccea7d79b2ca19426a5b
wfuertes22 <wf69@nau.edu>
2024-10-04 00:51:45 -0700

    added form for BookingUserInfo. Need to add CSS and still fix routing so that only BookingUserInfo or ReservationForm shows

--- coursera-final/src/components/Booking_Components/BookingUserInfo.js
+++ coursera-final/src/components/Booking_Components/BookingUserInfo.js
@@ -5,8 +5,36 @@ const BookingUserInfo = () => {
     const {fromReservationForm} = location.state;
 
     return(
-        <section className="container">
-            <p>{fromReservationForm.time}</p>
+        <section className="userInfoSection container">
+            <form>
+                <section>
+                    <label htmlFor="fName">First Name</label>
+                    <input name="fName" id="fName"></input>
+                </section>
+                <section>
+                    <label htmlFor="lName">Last Name</label>
+                    <input name="lName" id="lName"></input>
+                </section>
+                <section>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="email"></input>
+                </section>
+                <section>
+                    <label htmlFor="phone">Phone Number</label>
+                    <input type="tel" name="phone" id="phone"></input>
+                </section>
+                <section className="formData">
+                    <p>{fromReservationForm.reservationDate}</p>
+                    <p>{fromReservationForm.numOfDiners}</p>
+                    <p>{fromReservationForm.occasion}</p>
+                    <p>{fromReservationForm.time}</p>
+                </section>
+                <section>
+                    <label htmlFor="specialRequest">Special Requests</label>
+                    <input name="specialRequest" id="specialRequest"></input>
+                </section>
+            </form>
+            {/*<p>{fromReservationForm.time}</p>*/}
         </section>
     );
 }
