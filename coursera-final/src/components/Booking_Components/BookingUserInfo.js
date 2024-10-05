b42cd5a2070f2b9639de23de0566c11447b0d7ca
wfuertes22 <wf69@nau.edu>
2024-10-05 01:28:43 -0700

    added styling and grid layout to userInfoForm

--- coursera-final/src/components/Booking_Components/BookingUserInfo.js
+++ coursera-final/src/components/Booking_Components/BookingUserInfo.js
@@ -6,30 +6,30 @@ const BookingUserInfo = () => {
 
     return(
         <section className="userInfoSection container">
-            <form>
-                <section>
+            <form id="userInfoForm">
+                <section className="labelAndInput">
                     <label htmlFor="fName">First Name</label>
                     <input name="fName" id="fName"></input>
                 </section>
-                <section>
+                <section className="labelAndInput">
                     <label htmlFor="lName">Last Name</label>
                     <input name="lName" id="lName"></input>
                 </section>
-                <section>
+                <section className="labelAndInput"> 
                     <label htmlFor="email">Email</label>
                     <input type="email" name="email" id="email"></input>
                 </section>
-                <section>
+                <section className="labelAndInput">
                     <label htmlFor="phone">Phone Number</label>
                     <input type="tel" name="phone" id="phone"></input>
                 </section>
-                <section className="formData">
+                <section id="formData">
                     <p>{fromReservationForm.reservationDate}</p>
                     <p>{fromReservationForm.numOfDiners}</p>
                     <p>{fromReservationForm.occasion}</p>
                     <p>{fromReservationForm.time}</p>
                 </section>
-                <section>
+                <section className="labelAndInput">
                     <label htmlFor="specialRequest">Special Requests</label>
                     <input name="specialRequest" id="specialRequest"></input>
                 </section>
